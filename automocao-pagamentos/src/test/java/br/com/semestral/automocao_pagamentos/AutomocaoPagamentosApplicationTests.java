46030fca8ea9d9e3e8a29b87ca5d7298bf9dba46
Amanda Brito <112962708+amandalbrito@users.noreply.github.com>
2025-04-19 19:09:42 -0300

    Upload E-mail de confirmacao

---
+++ automocao-pagamentos/src/test/java/br/com/semestral/automocao_pagamentos/AutomocaoPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.semestral.automocao_pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomocaoPagamentosApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomocaoPagamentosApplicationTests.class, args);
+	}
+
+}
