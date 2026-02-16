07fd4d7b3fecc854efa631c43c5f51ee3212f282
amandalbrito <amandaluciabrito73@gmail.com.br>
2026-02-16 20:32:42 -0300

    Correção para deploy Render

--- src/main/java/br/com/fecaf/controller/railwayConnectionController.java
+++ src/main/java/br/com/fecaf/controller/railwayConnectionController.java
@@ -1,9 +1,12 @@
 package br.com.fecaf.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class railwayConnectionController {
+@RestController
 
+public class RailwayConnectionController {
+    
     @GetMapping("/")
     public String railwayConnection() {
         return "API está no ar!";
